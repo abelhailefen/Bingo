e76240c2b03f6ce5e2d3bac9a4bd38fe4506d9f2
Abel <abelhf@hilcoeschool.com>
2026-01-06 21:24:36 +0300

    program cs

--- Bingo Service/Bingo.API/Program.cs
+++ Bingo Service/Bingo.API/Program.cs
@@ -7,21 +7,39 @@ using Npgsql;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// 1. Configure Npgsql Data Source with Enums
+// 1. Get the connection string from Environment or Config
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-var dataSourceBuilder = new NpgsqlDataSourceBuilder(connectionString);
 
-// Map your C# enums to the Postgres types
+if (string.IsNullOrEmpty(connectionString))
+{
+    throw new Exception("Connection string is missing! Check Render Environment Variables.");
+}
+
+// 2. CONVERT URI FORMAT (postgres://) TO KEY-VALUE FORMAT
+// Render provides a URL, but .NET needs Key=Value
+if (connectionString.StartsWith("postgres://"))
+{
+    var databaseUri = new Uri(connectionString);
+    var userInfo = databaseUri.UserInfo.Split(':');
+
+    connectionString = $"Host={databaseUri.Host};Port={databaseUri.Port};Database={databaseUri.AbsolutePath.TrimStart('/')};Username={userInfo[0]};Password={userInfo[1]};SSL Mode=Require;Trust Server Certificate=true;";
+}
+
+// 3. Configure Npgsql Data Source with Enums
+var dataSourceBuilder = new NpgsqlDataSourceBuilder(connectionString);
 dataSourceBuilder.MapEnum<RoomStatusEnum>();
 dataSourceBuilder.MapEnum<WinPatternEnum>();
 dataSourceBuilder.MapEnum<WinTypeEnum>();
 
 var dataSource = dataSourceBuilder.Build();
 
-// 2. Add DbContext using the data source
+// 4. Add DbContext
 builder.Services.AddDbContext<BingoDbContext>(options =>
-    options.UseNpgsql(dataSource));
+    options.UseNpgsql(dataSource)
+           .UseSnakeCaseNamingConvention());
 
+// ... rest of your code (AddControllers, CORS, etc.)
+// REMOVE the second builder.Services.AddDbContext call at the bottom of your file
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
